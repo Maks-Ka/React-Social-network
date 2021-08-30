5abef6aaf01f6686f2ed6abc6b7ea9d89e09b5dc
Maks-KaNDer <maks2517.k@gmail.com>
2021-08-30 22:51:30 +0300

    design

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,7 @@ const Post = (props) => {
       <img src='https://sun2-10.userapi.com/s/v1/if1/LuadwtIw4jcZH1yvpORdN--H1fclrUYb7CsgV7ennIdgIke1PYraqTnJqD2A5S0cXHwuQA8-.jpg?size=50x0&quality=96&crop=58,0,339,339&ava=1' alt='' />
           { props.message }
       <div>
-        <span className={styles.text}>like 🧡</span> {props.likesCount}
+        <span className={styles.text}>like 🤍</span> {props.likesCount}
       </div>
     </div>
   )
